2a1d8eb51f8dc40a4edfe926942a9360308b033f
adamleduc <thinh.leduc@student.sps-cl.cz>
2023-09-18 14:22:17 +0200

    leduc

---
+++ 2.hodina/Strategypattern/Strategypattern/operation/operations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategypattern.operation
+{
+    internal class operations
+    {
+        public int execute(int a, int b)
+        {
+            return a - b; 
+        }
+    }
+}
